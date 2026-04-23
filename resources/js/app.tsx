d12fb322cabf24a1640bbd528850610c2f601226
Austin Drummond <adrum0x7@gmail.com>
2026-04-22 20:18:10 -0400

    chnge default title

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -16,7 +16,7 @@ configureEcho({
   broadcaster: "reverb",
 });
 
-const appName = import.meta.env.VITE_APP_NAME || "Laravel";
+const appName = import.meta.env.VITE_APP_NAME || "Envault";
 
 createInertiaApp({
   title: (title) => (title ? `${title} - ${appName}` : appName),
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,7 @@
         }
     </style>
 
-    <title inertia>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Envault') }}</title>
 
     <link rel="icon" href="/images/favicon.png" type="image/png" />
 

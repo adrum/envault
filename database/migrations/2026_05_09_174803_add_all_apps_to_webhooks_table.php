0200781968746928b2f56fe17b212864741ba113
Austin Drummond <adrum0x7@gmail.com>
2026-05-09 15:00:14 -0400

    added missing migration

---
+++ database/migrations/2026_05_09_174803_add_all_apps_to_webhooks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('webhooks', function (Blueprint $table) {
+            $table->boolean('all_apps')->default(false)->after('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('webhooks', function (Blueprint $table) {
+            $table->dropColumn('all_apps');
+        });
+    }
+};
